fdb8315b87e5685d56f3f603bdd403ba465a566c
gaiautoupload <gaiautoupload@gmail.com>
2026-08-11 15:53:24 +0800

    啟動前檢查並更新 Git 版本 (#1)

--- start.bat
+++ start.bat
@@ -1,2 +1,6 @@
+@echo off
+call "%~dp0git_update.bat"
+pushd "%~dp0"
 python main.py
+popd
 pause
--- start_main_no_round0.bat
+++ start_main_no_round0.bat
@@ -1,2 +1,6 @@
+@echo off
+call "%~dp0git_update.bat"
+pushd "%~dp0"
 python main_no_round0.py
+popd
 pause
